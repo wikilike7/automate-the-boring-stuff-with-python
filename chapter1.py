211687f7e7c7aae585d992c048e6b0cde3f31df3
wikilike7 <wikilike7@gmail.com>
2017-12-22 16:12:42 +0800

    add chapter1

---
+++ chapter1.py
@@ -0,0 +1,22 @@
+# the function of string, int, float
+str('ac')
+int('123')
+float(11)
+
+
+# define a variable
+name = 'zibba'
+print(name)
+
+
+# get input from user
+age = input('Please fill in your age: ')
+print(age)
+
+
+# statement vs expression
+sex = 'what is better for you?'  # statement
+1 + 2  # expression
+
+
+
