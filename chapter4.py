f924425331748d7a52f734c45d8004bdb24c37a3
wikilike7 <wikilike7@gmail.com>
2017-12-27 17:57:59 +0800

    update chapter4

--- chapter4.py
+++ chapter4.py
@@ -1,3 +1,5 @@
+import random
+
 # cat_names = []
 # while True:
 #     print('Enter the name of cat: ')
@@ -10,16 +12,30 @@
 #     print(name)
 
 
-supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
-for i in range(len(supplies)):
-    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+# supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
+# for i in range(len(supplies)):
+#     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+#
+#
+# pet_name = input('Please enter cat name: ')
+# pet_list = ['cat', 'dog', 'fish']
+# if pet_name in pet_list:
+#     print(pet_name + ' is my pet.')
+# else:
+#     print('I do not have a pet named ' + pet_name)
 
 
-pet_name = input('Please enter cat name: ')
-pet_list = ['cat', 'dog', 'fish']
-if pet_name in pet_list:
-    print(pet_name + ' is my pet.')
-else:
-    print('I do not have a pet named ' + pet_name)
+message = ['It is certain',
+           'It is decidedly so',
+           'Reply hazy try again',
+           'Ask again later',
+           'Concentrate and ask again',
+           'My reply is no',
+           'Outlook not so good',
+           'Very doubtful']
+
+# print(message[random.randint(0, len(message) - 1)])
 
 
+for i in message:
+    print(i)
